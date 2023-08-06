86c990ca9d46032d225587b72f12810e14d3b0a9
dev-ys-36 <tkddbs7631@naver.com>
2023-08-06 20:50:01 +0900

    detached & moved cse server

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ *    ____                  __________
+ *   / __ \_   _____  _____/ __/ / __ \_      __
+ *  / / / / | / / _ \/ ___/ /_/ / / / / | /| / /
+ * / /_/ /| |/ /  __/ /  / __/ / /_/ /| |/ |/ /
+ * \____/ |___/\___/_/  /_/ /_/\____/ |__/|__/
+ *
+ * The copyright indication and this authorization indication shall be
+ * recorded in all copies or in important parts of the Software.
+ *
+ * @github https://github.com/0verfl0w767
+ *
+ */
+const express = require('express')
+const fs = require('fs')
+const http = require('http')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(__dirname + '/page/status.html')
+})
+
+app.get('/status', (req, res) => {
+  res.redirect('https://basket.syu.kr/')
+})
+
+app.get('/statusData', (req, res) => {
+  const jsonData = JSON.parse(fs.readFileSync(__dirname + '/convert.json', 'utf8'))
+  res.status(200).json(jsonData)
+})
+
+app.get('*', (req, res) => {
+  res.status(404).json({statusCode: 404, message: 'unknown request.'})
+})
+
+http.createServer(app).listen(4646, '0.0.0.0')
